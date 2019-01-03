bf62fe2e544d32f818e267e08750b8b667955f85
Ryan Paul McClure <ryan333@vt.edu>
2019-01-03 09:30:07 -0700

    2018 CTD data packages uploaded to EDI
    Added the 2018 CTD data from FCR, BVR, and CCR. Also added funding sources from 2018.

--- Data/DataAlreadyUploadedToEDI/EDIProductionFiles/MakeEMLCTD/MakeEMLCTD.R
+++ Data/DataAlreadyUploadedToEDI/EDIProductionFiles/MakeEMLCTD/MakeEMLCTD.R
@@ -1,5 +1,3 @@
-setwd("/Users/cayelan/Dropbox/ComputerFiles/Virginia_Tech/Falling Creek/DataForWebsite/Github/ReservoirData/Data/DataAlreadyUploadedToEDI/EDIProductionFiles/MakeEMLCTD")
-
 # Install and load devtools
 install.packages("devtools")
 library(devtools)
@@ -9,23 +7,21 @@ install_github("EDIorg/EMLassemblyline")
 library(EMLassemblyline)
 
 # Import Templates ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-import_templates(path = "/Users/cayelan/Dropbox/ComputerFiles/Virginia_Tech/Falling Creek/DataForWebsite/Github/ReservoirData/Data/DataAlreadyUploadedToEDI/EDIProductionFiles/MakeEMLCTD", 
+import_templates(path = "C:/Users/Owner/Dropbox/MakeEMLCTD", 
                  license = "CCBY", 
-                 data.files = c("CTD_Meta_13_17.csv"))
+                 data.files = "CTD_Meta_13_18_final.csv")
 
 # Define Categorical Variables ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-define_catvars(path = "/Users/cayelan/Dropbox/ComputerFiles/Virginia_Tech/Falling Creek/DataForWebsite/Github/ReservoirData/Data/DataAlreadyUploadedToEDI/EDIProductionFiles/MakeEMLCTD")
+define_catvars(path = "C:/Users/Owner/Dropbox/MakeEMLCTD")
 
 # Make the EML for EDI ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#path = "C:/Users/Owner/Desktop/EDI_CTD_upload"
-
-make_eml(path = "/Users/cayelan/Dropbox/ComputerFiles/Virginia_Tech/Falling Creek/DataForWebsite/Github/ReservoirData/Data/DataAlreadyUploadedToEDI/EDIProductionFiles/MakeEMLCTD",
-         dataset.title = "Time series of high-frequency profiles of depth, temperature, dissolved oxygen, conductivity, specific conductivity, chlorophyll a, turbidity, pH, and oxidation-reduction potential for Beaverdam Reservoir, Carvins Cove Reservoir, Falling Creek Reservoir, Gatewood Reservoir, and Spring Hollow Reservoir in Southwestern Virginia, USA 2013-2017",
-         data.files = "CTD_Meta_13_17.csv",
+make_eml(path = "C:/Users/Owner/Dropbox/MakeEMLCTD",
+         dataset.title = "Time series of high-frequency profiles of depth, temperature, dissolved oxygen, conductivity, specific conductivity, chlorophyll a, turbidity, pH, oxidation-reduction potential, photosynthetic active radiation, and descent rate for Beaverdam Reservoir, Carvins Cove Reservoir, Falling Creek Reservoir, Gatewood Reservoir, and Spring Hollow Reservoir in Southwestern Virginia, USA 2013-2018",
+         data.files = "CTD_Meta_13_18_final.csv",
          data.files.description = "Reservoir CTD dataset",
          data.files.quote.character = "\"",
-         temporal.coverage = c("2013-03-07", "2017-12-10"),
-         #geographic.description = "Southwestern Virginia, USA, North America", # This argument is only required if bounding_boxes.txt is absent.
-         maintenance.description = "ongoing", 
-         user.id = "carylab7",
-         package.id = "edi.200.3")
+         temporal.coverage = c("2013-03-07", "2018-12-17"),
+         maintenance.description = "ongoing",
+         affiliation = "EDI",
+         user.id = "ccarey",
+         package.id = "edi.200.6")
