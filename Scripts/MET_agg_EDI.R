d39b70c041a6a0526f86fc85e7ea6ac2d1e82bba
bethanybookout <38041971+bethanybookout@users.noreply.github.com>
2019-01-10 12:40:38 -0500

    Met aggregation file

---
+++ Scripts/MET_agg_EDI.R
@@ -0,0 +1,84 @@
+###EDI MET STATION File
+##Let's get the data together and looking good
+#Probably should put this in a Git workflow friendly format.. oh well.
+
+#packages needed
+library("lubridate")
+install.packages("tidyverse")
+library(tidyverse)
+
+setwd("/Users/bethany1/Desktop/MET_EDI/") #need to make into a relative path for EDI folder..
+
+Met_past=read.csv("AllRawMetData_20181119.csv", sep = ",") #loads in data from FCR_GLM repository
+Met_past$TIMESTAMP=ymd_hms(Met_past$TIMESTAMP, tz="Etc/GMT+4")
+#Met_past$X<-NULL
+Met_now=read.csv("https://raw.githubusercontent.com/CareyLabVT/SCCData/carina-data/FCRmet.csv", skip = 4, header = F) #loads in data from SCC_data repository for latest push
+if(length(names(Met_now))>17){ #removes NR01TK_Avg column, which was downloaded on some but not all days
+  Met_now$V17<-NULL #remove extra column
+}
+names(Met_now) = c("TIMESTAMP","RECORD","BattV","PTemp_C","PAR_Den_Avg","PAR_Tot_Tot","BP_kPa_Avg","AirTC_Avg","RH","Rain_mm_Tot","WS_ms_Avg","WindDir","SR01Up_Avg","SR01Dn_Avg","IR01UpCo_Avg","IR01DnCo_Avg","Albedo_Avg")
+Met_now$TIMESTAMP=ymd_hms(Met_now$TIMESTAMP, tz="Etc/GMT+4")
+Met_now=Met_now[-c(which(is.na(Met_now$TIMESTAMP))),] #checks for NA from failed parse, and deletes from dataset
+Met_now$PAR_Tot_Tot=as.numeric(Met_now$PAR_Tot_Tot)
+#str(Met_now); str(Met_past) #checks structure for matching
+#renames rows for easy bind
+
+Met_agg<-rbind(Met_past,Met_now) #binds past and current data from Met station
+#Met=Met[-c(which(duplicated(Met))),] #removes duplicate data, takes a long time... unique() is also an option
+Met= distinct(Met_agg) #tidy version, identifies duplicated rows and saves first row, removing any duplicates
+length(which(is.na(Met))) #check for rows that are only NA
+
+##Fix Time
+#a couple options for timezones GMT -4 or America/Puerto_Rico or any timezone that does not change for daylight.
+#Met_timetest=ymd_hms(Met_now$TIMESTAMP, tz="America/Puerto_Rico")
+#Met_time2=ymd_hms(Met_now$TIMESTAMP, tz="Etc/GMT+4")
+#OlsonNames() #valid time zones, wiki link to what these mean: https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+
+Met$TIMESTAMP=ymd_hms(Met$TIMESTAMP, tz="Etc/GMT+4") #resulted in 1 failed parse
+
+
+###Time to do the hard part. Cleaning up the data.###
+
+#check record
+for(i in 2:length(Met$RECORD)){ #this identifies if there are any data gaps in the long-term record, and where they are by record number
+  if(Met$RECORD[i]-Met$RECORD[i-1]>1){
+    print(c(Met$TIMESTAMP[i-1],Met$TIMESTAMP[i]))
+  }
+}
+
+###PAR
+
+##Barometric Pressure
+#remove negative values
+
+###AirTemp
+#remove >
+
+###Relative Humidity
+#remove negative values
+#remove >100
+Met$RH=
+
+###Rainfall
+#remove negative values
+
+###Wind speed + direction
+#remove negative values
+
+###Short wave radiation
+
+###Long wave radiation
+
+###Albedo???
+
+###Remove maintenance datetime
+
+##Format data for EDI##
+Met$Site=50 #add site
+Met$Reservoir= "FCR"#add reservoir
+Met$Flag= 0 #add flags
+#add notes for flags?? still think about
+
+#fix column names and order
+Met=Met[,c(18:20,1:17)] #fixes order, does not fix names yet
+names(Met_now) = c("Site", "Reservoir", "DateTime","Record") #fill out rest of col names when finalized
