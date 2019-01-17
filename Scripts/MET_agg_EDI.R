cefaef061f5777e786f8293e2175b8ecadf48a03
bethany.bookout <bethanyb18@vt.edu>
2019-01-17 11:08:47 -0500

    clean up and reorder data aggregration

--- Scripts/MET_agg_EDI.R
+++ Scripts/MET_agg_EDI.R
@@ -11,35 +11,28 @@ setwd("/Users/bethany1/Desktop/MET_EDI/") #need to make into a relative path for
 
 Met_past=read.csv("AllRawMetData_20181119.csv", sep = ",") #loads in data from FCR_GLM repository
 Met_past$TIMESTAMP=ymd_hms(Met_past$TIMESTAMP, tz="Etc/GMT+4")
-#Met_past$X<-NULL
+
 Met_now=read.csv("https://raw.githubusercontent.com/CareyLabVT/SCCData/carina-data/FCRmet.csv", skip = 4, header = F) #loads in data from SCC_data repository for latest push
 if(length(names(Met_now))>17){ #removes NR01TK_Avg column, which was downloaded on some but not all days
   Met_now$V17<-NULL #remove extra column
 }
+#renames and reformats dataset for easy bind
 names(Met_now) = c("TIMESTAMP","RECORD","BattV","PTemp_C","PAR_Den_Avg","PAR_Tot_Tot","BP_kPa_Avg","AirTC_Avg","RH","Rain_mm_Tot","WS_ms_Avg","WindDir","SR01Up_Avg","SR01Dn_Avg","IR01UpCo_Avg","IR01DnCo_Avg","Albedo_Avg")
 Met_now$TIMESTAMP=ymd_hms(Met_now$TIMESTAMP, tz="Etc/GMT+4")
 Met_now=Met_now[-c(which(is.na(Met_now$TIMESTAMP))),] #checks for NA from failed parse, and deletes from dataset
 Met_now$PAR_Tot_Tot=as.numeric(Met_now$PAR_Tot_Tot)
 #str(Met_now); str(Met_past) #checks structure for matching
-#renames rows for easy bind
 
 Met_agg<-rbind(Met_past,Met_now) #binds past and current data from Met station
-#Met=Met[!duplicated(Met),] #removes duplicate data, takes a long time... unique() is also an option
 
 Met= Met_agg
-Met$TIMESTAMP=ymd_hms(Met$TIMESTAMP, tz="Etc/GMT+4") #resulted in 1 failed parse
-#Met %>% distinct(TIMESTAMP, RECORD, .keep_all = TRUE)
-#why doesn't this work??? UGH Need to fix
-Met = Met[!duplicated(Met$TIMESTAMP),]
+Met$TIMESTAMP=ymd_hms(Met$TIMESTAMP, tz="Etc/GMT+4") #formats timestamp
+#resulted in 1 failed parse
+
+Met = Met[!duplicated(Met$TIMESTAMP),] #takes out duplicated values by timestamp
 
 length(which(is.na(Met))) #check for rows that are only NA
-Met=Met[-c(which(is.na(Met))),]
-##Fix Time
-#a couple options for timezones GMT -4 or America/Puerto_Rico or any timezone that does not change for daylight.
-#Met_timetest=ymd_hms(Met_now$TIMESTAMP, tz="America/Puerto_Rico")
-#Met_time2=ymd_hms(Met_now$TIMESTAMP, tz="Etc/GMT+4")
-#OlsonNames() #valid time zones, wiki link to what these mean: https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
-
+Met=Met[-c(which(is.na(Met))),] #doesn't seem to actually work..
 
 ###Time to do the hard part. Cleaning up the data.###
 
