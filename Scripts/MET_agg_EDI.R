567e778ba6f76735ad034e7a45a9c3e6b2c73c1d
bethany.bookout <bethanyb18@vt.edu>
2019-01-17 18:42:40 -0500

    completed for loop for flag 1!

--- Scripts/MET_agg_EDI.R
+++ Scripts/MET_agg_EDI.R
@@ -261,19 +261,19 @@ plot(Met$TIMESTAMP, Met$IR01DnCo_Avg, type = 'l')
 #Plot for flags 2&3
 plot(Met$TIMESTAMP, Met$Flag, type = 'p')
 
-#Flag 1
-Met2=Met
-#gonna try a for loop
+#Flag 1 & 4
+#for loop inserts flags and notes, then sets relevant data to NA
 for (i in 1:nrow(RemoveMet)){ #makes i # of rows in Maintenance log
   #plug in flag and notes from timeframe
   Met2$Flag[Met2$TIMESTAMP>=RemoveMet$TIMESTAMP_start[i] & Met2$TIMESTAMP<=RemoveMet$TIMESTAMP_end[i]]=RemoveMet$flag[i]
   Met2$Notes[Met2$TIMESTAMP>=RemoveMet$TIMESTAMP_start[i] & Met2$TIMESTAMP<=RemoveMet$TIMESTAMP_end[i]]=RemoveMet$notes[i] 
-}
+  #if flag == 1, set parameter to NA
+  if(RemoveMet$flag[i]==1)
+  {Met2[Met2$TIMESTAMP>=RemoveMet$TIMESTAMP_start[i] & Met2$TIMESTAMP<=RemoveMet$TIMESTAMP_end[i],
+        RemoveMet$colnumber[i]] = NA}
+  }
 
-#if flag == 1, set paramter to NA
-time_remove=RemoveMet[,c(2:3)]
-sub_remove=RemoveMet[,c(4:5)]
-flip_remove=data.frame(t(sub_remove))
+
 #fix column names and order
 Met_final=Met[,c(18:19,1:17,20:21)] #fixes order, does not fix names yet
 names(Met_final) = c("Site", "Reservoir", "DateTime","Record", "CR3000_Batt_V", "CR3000Panel_temp_C", 
